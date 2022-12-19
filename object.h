269012f77e005450e651f23336ba286bf91437ab
Mic Pringle <michael_pringle@apple.com>
2022-12-19 10:58:49 +0000

    Chapter 24, section 1

--- object.h
+++ object.h
@@ -6,16 +6,20 @@
 #define CLOX_OBJECT_H
 
 #include "common.h"
+#include "chunk.h"
 #include "value.h"
 
 #define OBJ_TYPE(value) (AS_OBJ(value)->type)
 
+#define IS_FUNCTION(value) isObjType(value, OBJ_FUNCTION)
 #define IS_STRING(value) isObjType(value, OBJ_STRING)
 
+#define AS_FUNCTION(value) ((ObjFunction *) AS_OBJ(value))
 #define AS_STRING(value) ((ObjString *) AS_OBJ(value))
 #define AS_CSTRING(value) (((ObjString *) AS_OBJ(value))->chars)
 
 typedef enum {
+    OBJ_FUNCTION,
     OBJ_STRING,
 } ObjType;
 
@@ -24,12 +28,21 @@ struct Obj {
     struct Obj *next;
 };
 
+typedef struct {
+    Obj obj;
+    int arity;
+    Chunk chunk;
+    ObjString *name;
+} ObjFunction;
+
 struct ObjString {
     Obj obj;
     int length;
     char *chars;
     uint32_t hash;
 };
+
+ObjFunction *newFunction();
 
 ObjString *takeString(char *chars, int length);
 
